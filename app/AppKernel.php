317333ceb6830c60d173e7db82f3e23c5e4150b6
Théo Guérin <theo.guerin.pro@gmail.com>
2015-11-21 22:00:20 +0100

    supression du AppBundle (inutile)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new ProjetVoxel\EmploiBundle\ProjetVoxelEmploiBundle(),
             new ProjetVoxel\UserBundle\ProjetVoxelUserBundle(),
             new ProjetVoxel\CoreBundle\ProjetVoxelCoreBundle(),
