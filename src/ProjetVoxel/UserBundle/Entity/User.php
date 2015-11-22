14cef47da8b50ed7e069a4ad716e68625ccbc152
Théo Guérin <theo.guerin.pro@gmail.com>
2015-11-22 15:26:18 +0100

    Update de la classe user pour heriter de celle de FOSUB

--- src/ProjetVoxel/UserBundle/Entity/User.php
+++ src/ProjetVoxel/UserBundle/Entity/User.php
@@ -3,38 +3,17 @@
 namespace ProjetVoxel\UserBundle\Entity;
 
 use Symfony\Component\Security\Core\User\UserInterface;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * User
  */
-class User implements UserInterface
+class User extends BaseUser
 {
     /**
      * @var integer
      */
-    private $id;
-
-    /**
-     * @var string
-     * @Column(unique=true)
-     */
-    private $username;
-
-    /**
-     * @var string
-     */
-    private $password;
-
-    /**
-     * @var string
-     */
-    private $salt;
-
-    /**
-     * @var array
-     */
-    private $roles = array();
-
+    protected $id;
 
     /**
      * Get id
@@ -46,103 +25,4 @@ class User implements UserInterface
         return $this->id;
     }
 
-    /**
-     * Set username
-     *
-     * @param string $username
-     *
-     * @return User
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return string
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
-
-    /**
-     * Set password
-     *
-     * @param string $password
-     *
-     * @return User
-     */
-    public function setPassword($password)
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Get password
-     *
-     * @return string
-     */
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Set salt
-     *
-     * @param string $salt
-     *
-     * @return User
-     */
-    public function setSalt($salt)
-    {
-        $this->salt = $salt;
-
-        return $this;
-    }
-
-    /**
-     * Get salt
-     *
-     * @return string
-     */
-    public function getSalt()
-    {
-        return $this->salt;
-    }
-
-    /**
-     * Set roles
-     *
-     * @param array $roles
-     *
-     * @return User
-     */
-    public function setRoles($roles)
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
-     * Get roles
-     *
-     * @return array
-     */
-    public function getRoles()
-    {
-        return $this->roles;
-    }
-
-    public function eraseCredentials()
-    {
-    }
 }
