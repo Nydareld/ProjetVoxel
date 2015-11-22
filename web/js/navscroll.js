03614217cc4b84ea96b4544d937cc4a7a5e0f292
Théo Guérin <theo.guerin.pro@gmail.com>
2015-11-22 20:51:48 +0100

    Bandeau de connexion étendu se cache en scrollant,le réduit s'affiche

--- web/js/navscroll.js
+++ web/js/navscroll.js
@@ -4,9 +4,13 @@ jQuery(document).ready(function() {
   jQuery(window).bind('scroll', function() {
       if (jQuery(window).scrollTop() > navpos.top-40) {
           jQuery('#navbar1').addClass('navbar-fixed-top');
+          jQuery('#bandeauCoReduit').removeClass('hidden');
+          jQuery('#bandeauCo').addClass('hidden');
        }
        else {
            jQuery('#navbar1').removeClass('navbar-fixed-top');
+           jQuery('#bandeauCo').removeClass('hidden');
+           jQuery('#bandeauCoReduit').addClass('hidden');
        }
     });
 });
