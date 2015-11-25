21ecc088a741fc28e1a88d02a81addc41b63d1b0
Théo Guérin <theo.guerin.pro@gmail.com>
2015-11-25 14:37:35 +0100

    Adaptation de la classe user pour l'upload d'avatar

--- src/ProjetVoxel/UserBundle/Entity/User.php
+++ src/ProjetVoxel/UserBundle/Entity/User.php
@@ -3,6 +3,8 @@
 namespace ProjetVoxel\UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -47,36 +49,130 @@ use Doctrine\ORM\Mapping as ORM;
      private $ownedCompany;
 
      /**
-      * Get id
-      *
-      * @return integer
+      * @ORM\Column(type="string", length=255, nullable=true)
       */
-     public function getId()
-     {
-         return $this->id;
-     }
+    protected $path;
 
-     public function getCreatedCompany(){
-         return $this->CreatedCompany;
-     }
+    /**
+     * @Assert\File(maxSize="1000000")
+     */
+    private $file;
 
-     public function setCreatedCompany($CreatedCompany){
-         $this->CreatedCompany = $CreatedCompany;
-     }
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-     public function getManagedCompany(){
-         return $this->managedCompany;
-     }
+    public function getCreatedCompany(){
+        return $this->CreatedCompany;
+    }
 
-     public function setManagedCompany($managedCompany){
-         $this->managedCompany = $managedCompany;
-     }
+    public function setCreatedCompany($CreatedCompany){
+        $this->CreatedCompany = $CreatedCompany;
+    }
 
-     public function getOwnedCompany(){
-         return $this->ownedCompany;
-     }
+    public function getManagedCompany(){
+        return $this->managedCompany;
+    }
 
-     public function setOwnedCompany($ownedCompany){
-         $this->ownedCompany = $ownedCompany;
-     }
- }
+    public function setManagedCompany($managedCompany){
+        $this->managedCompany = $managedCompany;
+    }
+
+    public function getOwnedCompany(){
+        return $this->ownedCompany;
+    }
+
+    public function setOwnedCompany($ownedCompany){
+        $this->ownedCompany = $ownedCompany;
+    }
+
+    public function getPath(){
+        return $this->path;
+    }
+
+    public function setPath($path){
+        $this->path = $path;
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->path;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->path
+            ? null
+            : '/'.$this->getUploadDir().'/'.$this->path;
+    }
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/avatars';
+    }
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function upload()
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and then the
+        // target filename to move to
+
+        $extension = $this->file->guessExtension();
+        $fileName = $this->username.'.'.$extension;
+
+        $this->getFile()->move(
+            $this->getUploadRootDir(),
+            $fileName
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->path = $fileName;
+
+        // clean up the file property as you won't need it anymore
+        $this->file = null;
+    }
+
+}
