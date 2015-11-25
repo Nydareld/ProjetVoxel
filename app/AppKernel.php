49cbd4011d9b2e3623b81d3b5da289a78e03d8e6
Nayos <sainthetique6@gmail.com>
2015-11-25 15:41:17 +0100

    creation du bundle Economy et création de la route

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new ProjetVoxel\EmploiBundle\ProjetVoxelEmploiBundle(),
             new ProjetVoxel\UserBundle\ProjetVoxelUserBundle(),
             new ProjetVoxel\CoreBundle\ProjetVoxelCoreBundle(),
+            new ProjetVoxel\EconomyBundle\ProjetVoxelEconomyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
