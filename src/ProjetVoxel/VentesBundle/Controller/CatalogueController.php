b82c6d43b7482cf50ea10abc466d1718aa3beb64
Théo Guérin <theo.guerin.pro@gmail.com>
2015-12-09 19:11:32 +0100

    controlleur d'ajout d'item au catalogue

--- src/ProjetVoxel/VentesBundle/Controller/CatalogueController.php
+++ src/ProjetVoxel/VentesBundle/Controller/CatalogueController.php
@@ -2,9 +2,55 @@
 
 namespace ProjetVoxel\VentesBundle\Controller;
 
+use ProjetVoxel\EconomyBundle\Entity\BankId;
+use Symfony\Component\Security\Acl\Exception\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use ProjetVoxel\VentesBundle\Entity\CatalogueItem;
+use ProjetVoxel\VentesBundle\Form\CatalogueItemType;
 
 class CatalogueController extends Controller
 {
+    public function addItemAction($bankId)
+    {
+        // Si le type est pas co
+        if (!$this->get('security.context')->isGranted('ROLE_USER')) {
+            // Sinon on déclenche une exception « Accès interdit »
+            throw new AccessDeniedException('Accès limité aux utilisateurs');
+        }
 
+        // Récupération de l'user et du bank id
+        $bankId = $this->getDoctrine()->getManager()->getRepository('ProjetVoxelEconomyBundle:BankId')->findOneBy(array('id' => $bankId ));
+        $user = $this->get('security.context')->getToken()->getUser();
+        if ($bankId == null) {
+            throw new Exception("Cet identité bancaire n'existe pas", 1);
+
+        }
+        // Vérification des droits du user sur le bankId ou la company
+        if (! $bankId->userHasWright($user) ) {
+            throw new AccessDeniedException('Accès limité aux utilisateurs de cet identité bancaire ou aux gérants de son entreprise');
+        }
+        // Création du formulaire de nouvel item
+
+        $request = $this->get('request');
+        $item = new CatalogueItem();
+        $form = $this->get('form.factory')->create(new CatalogueItemType(), $item);
+
+        if ($form->handleRequest($request)->isValid()) {
+
+            $item->setBankId($bankId);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($item);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Entitée bien ajoutée au catalogue');
+
+            return $this->redirect($this->generateUrl('projet_voxel_ventes_catalogue', array('bankId' => $bankId->getId())));
+
+        }
+        return $this->render('ProjetVoxelVentesBundle:Catalogue:addItem.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
 }
