3fa8228987a41010630471865b78637e76eeb83b
Théo Guérin <theo.guerin.pro@gmail.com>
2016-01-03 15:50:00 +0100

    ajout d'un exemple pour les notifications dans le defautController du coreBundle

--- src/ProjetVoxel/CoreBundle/Controller/DefaultController.php
+++ src/ProjetVoxel/CoreBundle/Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace ProjetVoxel\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ProjetVoxel\UserBundle\Entity\Notification;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        /* Exemple Notification
+        $notif = new Notification();
+        $notif->setMessage("Zbra");
+        $notif->setRoute("projet_voxel_emploi_editEntreprise");
+        $notif->setArguments(array('id' => 8 ));
+        $notif->setUser($this->get('security.context')->getToken()->getUser());
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($notif);
+        $em->flush();
+        */
         return $this->render('ProjetVoxelCoreBundle:Default:index.html.twig');
     }
 }
